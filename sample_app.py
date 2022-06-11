35ab72fa6e6b51c5581ecb93c93cb06cac383139
latototita <85679433+latototita@users.noreply.github.com>
2022-06-10 19:40:05 -0500

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -51,4 +51,4 @@ def error404(error):
 if os.environ.get('APP_LOCATION') == 'heroku':
     run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
-    run(host='localhost', port=8080, debug=True)
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
